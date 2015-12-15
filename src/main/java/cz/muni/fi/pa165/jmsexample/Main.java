416720cac194366ba1c2f52095861ffb89f7b372
Michal Bábel <410434@mail.muni.cz>
2015-12-15 15:49:16 +0100

    Step 4

--- src/main/java/cz/muni/fi/pa165/jmsexample/Main.java
+++ src/main/java/cz/muni/fi/pa165/jmsexample/Main.java
@@ -1,81 +1,31 @@
 package cz.muni.fi.pa165.jmsexample;
 
-import javax.jms.*;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import javax.jms.Session;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 
+@SpringBootApplication
+@EnableJms
 public class Main {
 
-    static class Consumer implements Runnable {
-
-        @Override
-        public void run() {
-            try {
-                InitialContext jndi = new InitialContext();
-                ConnectionFactory connectionFactory = (ConnectionFactory) jndi.lookup("connectionFactory");
-                Connection connection = connectionFactory.createConnection();
-                connection.start();
-                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                Destination destination = (Destination) jndi.lookup("Task2.HelloWorldQueue");
-                MessageConsumer consumer = session.createConsumer(destination);
-                Message message = consumer.receive();
-
-                if (message instanceof TextMessage) {
-                    TextMessage textMessage = (TextMessage) message;
-                    String text = textMessage.getText();
-                    System.out.println("Received: " + text);
-                }
-
-                consumer.close();
-                session.close();
-                connection.close();
-
-            } catch (JMSException ex) {
-                System.err.println("Problem with JMS: " + ex);
-            } catch (NamingException ex) {
-                System.err.println("Problem with JDNI: " + ex);
-            }
-
-        }
-
-    }
-
-    static class Producer implements Runnable {
-
-        @Override
-        public void run() {
-            try {
-                InitialContext jndi = new InitialContext();
-                ConnectionFactory connectionFactory = (ConnectionFactory) jndi.lookup("connectionFactory");
-                Connection connection = connectionFactory.createConnection();
-                connection.start();
-                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                Destination destination = (Destination) jndi.lookup("Task2.HelloWorldQueue");
-                MessageProducer producer = session.createProducer(destination);
-                producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-                String text = "Hello world!";
-                TextMessage message = session.createTextMessage(text);
-                System.out.println("Sent message: " + message.getText());
-                producer.send(message);
-                session.close();
-                connection.close();
-                session.close();
-                connection.close();
-            } catch (JMSException ex) {
-                System.err.println("Problem with JMS: " + ex);
-            } catch (NamingException ex) {
-                System.err.println("Problem with JDNI: " + ex);
-            }
-        }
-
-    }
-
     public static void main(String[] args) {
+        // Launch the application
+        ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
 
-        Thread t1 = new Thread(new Consumer());
-        Thread t2 = new Thread(new Producer());
-
-        t1.start();
-        t2.start();
+        // Send a message
+        MessageCreator helloMessageCreator = (Session session) -> session.createTextMessage("Hello");
+        MessageCreator worldMessageCreator = (Session session) -> session.createTextMessage("World!");
+        
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        System.out.println("Sending the hello message.");
+        jmsTemplate.send("hello-destination", helloMessageCreator);
+        System.out.println("Sending the world message.");
+        jmsTemplate.send("world-destination", worldMessageCreator);
+        
+        //TODO: Create 2 subscribers (in spring named receivers) that subscibe to defined topics.
     }
 }
